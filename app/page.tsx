c81f5773020e6e15688a3b0ab821798b68292083
ZdravkoRistic <easycodemaster@gmail.com>
2023-11-02 16:00:11 +0100

    finshed Camp component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <Camp />
+      <Camp />
       <Guide />
       <Features />
-      <GetApp /> */}
+      <GetApp />
     </>
   );
 }
